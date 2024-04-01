706519413a4805034174b6bf34b405caee1c29f9
Sarthak Gupta <sarthak.writter1@gmail.com>
2024-04-01 18:46:11 +0530

    port removed

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const btcusdRouter = require('./routes/btcusd.routes');
 const eurusdRouter = require('./routes/eurusd.routes');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
@@ -25,6 +24,4 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use('/btcusd', btcusdRouter);
 app.use('/eurusd', eurusdRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+
