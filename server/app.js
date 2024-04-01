adcbf72c69e70ee304571936bafef4894d2866ec
Sarthak Gupta <65368603+InsaneSparta@users.noreply.github.com>
2024-04-01 18:06:40 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  origin: ["https://candle-stick-graph.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+));
 app.use(express.json());
 
 // MongoDB connection
